f853dae83eebc00b829679004da95511ec32cb62
gary1030 <gary6658@gmail.com>
2022-12-22 12:15:01 +0000

    test

--- zenoh-measurement/z_sub.py
+++ zenoh-measurement/z_sub.py
@@ -101,7 +101,7 @@ sub = session.declare_subscriber(
     key, listener, reliability=Reliability.RELIABLE())
 
 
-time.sleep(360)
+time.sleep(160)
 print("Ending subscription...")
 
 # Cleanup: note that even if you forget it, cleanup will happen automatically when
