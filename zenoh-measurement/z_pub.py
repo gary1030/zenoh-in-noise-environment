2bfb4aaf8271b3203eabb34965824d9c41d7ba96
gary1030 <gary6658@gmail.com>
2022-12-22 12:38:35 +0000

    test

--- zenoh-measurement/z_pub.py
+++ zenoh-measurement/z_pub.py
@@ -91,14 +91,14 @@ start_time = int(time.time() * 1000)
 
 for idx in range(100):
     print(f"The sent packet id: {idx}")
-    sent_time = int(time.time() * 1000)
+    sent_time = int(time.time() * 1000000)
     sent_packet_info_list.append({"packet_id": -1, "sent_time": sent_time})
     pub.put(f"{idx},{paylod_with_1MB}")
 
 
 for idx in itertools.count() if args.iter is None else range(args.iter):
     print(f"The sent packet id: {idx}")
-    sent_time = int(time.time() * 1000)
+    sent_time = int(time.time() * 1000000)
     sent_packet_info_list.append({"packet_id": idx, "sent_time": sent_time})
     pub.put(f"{idx},{paylod_with_1MB}")
     time.sleep(0.01)
